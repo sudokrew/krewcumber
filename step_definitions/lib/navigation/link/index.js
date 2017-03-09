56e32f674d211ab1d94afd97660c0c28a82a621f
Tony Gaskell <tony@sudokrew.com>
2017-03-08 21:54:26 -1000

    Refactor click link to use click element method

--- step_definitions/lib/navigation/link/index.js
+++ step_definitions/lib/navigation/link/index.js
@@ -1,15 +1,17 @@
+var interaction = require('../../interaction');
+
 module.exports = {
   validateAnchorText: validateAnchorText,
   click: click,
 };
 
 /**
- *
+ * Clicks an anchor element with the given anchor text
  * @param  {WebdriverIO} browser Instance of web driver
  * @param  {String} anchorText   The anchor text to target by
  */
 function click (browser, anchorText) {
-  return browser.click('a=' + anchorText);
+  return interaction.clickElement(browser, 'a=' + anchorText);
 }
 
 /**
